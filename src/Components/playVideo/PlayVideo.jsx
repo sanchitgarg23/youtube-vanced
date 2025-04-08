524be43dad47df5bc1b0100bea8e75a33dddf5ee
Sanchit Garg <sanchit.garg2024@nst.rishihood.edu.in>
2025-04-08 19:28:20 +0530

    css issue fix

--- src/Components/playVideo/PlayVideo.jsx
+++ src/Components/playVideo/PlayVideo.jsx
@@ -174,7 +174,7 @@
 
 
 import React, { useEffect, useState } from 'react'
-import './PlayVideo.css'
+import './playVideo.css'
 import like from '../../assets/like.png'
 import dislike from '../../assets/dislike.png'
 import share from '../../assets/share.png'
